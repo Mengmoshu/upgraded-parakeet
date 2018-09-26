50ab633ee706eb8c9f78eab26066a85599ad4d24
Mengmoshu <mengmoshu@gmail.com>
2018-09-26 00:49:33 -0700

    Merged Layout class into Panel class. No reason to split them unless each style is a class, in which case a generic Layout class would still be redundant.

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ print("Hello World")
 
 class Panel:
 
-    def __init__(self, parent, content, width=1, height=1):
+    def __init__(self, parent, style, content, width=1, height=1):
         """
-
+        Styles: simpleGrid, columns, rows, box, columnSplit
+        :type style: str
         :type width: int
         :type height: int
         """
         self.parent = parent
+        self.style = style
         self.content = content
         self.width = width
         self.height = height
@@ -33,14 +35,3 @@ class DisplayTerminal:
         self.height = height
         self.owner = "dummyPlayer"  # TODO: Multiplayer
         self.contents = list()
-
-
-class Layout(panel):
-
-    def __init__(self, parent, style, content, width, height):
-        """
-        Styles: simpleGrid, columns, rows, box, columnSplit
-        :type style: str
-        """
-        super(Layout, self).__init__(parent, content, width, height)
-        self.style = style
